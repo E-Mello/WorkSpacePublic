532e0caf477ef75941a6cb99d2ab586854a425eb
root <root@DESKTOP-2BN3CML.localdomain>
2022-03-28 16:08:59 -0400

    Add Files

---
+++ MeuSite/js/script.js
@@ -0,0 +1,10 @@
+// Comando para chamar o slide show de forma simples
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    fade: true,
+    autoplay: true
+});
