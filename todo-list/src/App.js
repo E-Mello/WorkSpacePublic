227314db56e452f97f30b6587abf8d2a023a9c76
E-Mello <e.mello.dev@gmail.com>
2022-03-30 16:42:13 -0400

    End To.Do Project

---
+++ todo-list/src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css';
+import TodoList from './components/ToDoList.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div className="App">
+        <TodoList />
+    </div>
+  );
+}
+
+export default App;
